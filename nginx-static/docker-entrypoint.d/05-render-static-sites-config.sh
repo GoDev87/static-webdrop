c0784bc67238819b2532279f596c6ad2c1fe1ee3
Cluselaud Lucas <lulu.oleron.lc@gmail.com>
2026-06-16 21:40:32 +0200

    Fix server_name not escaped

--- nginx-static/docker-entrypoint.d/05-render-static-sites-config.sh
+++ nginx-static/docker-entrypoint.d/05-render-static-sites-config.sh
@@ -3,13 +3,13 @@ set -eu
 
 : "${SITES_BASE_DOMAIN:?SITES_BASE_DOMAIN is required, for example sites.example.com}"
 
-# Escape dots and other regex metacharacters because the value is used in a
-# regex server_name. Example: sites.example.com -> sites\.example\.com
-SITES_BASE_DOMAIN_REGEX=$(printf '%s' "$SITES_BASE_DOMAIN" | sed 's/[.[\*^$()+?{}|\\]/\\&/g')
+# The base domain is used inside an Nginx regex. Escape dots so
+# test.local becomes test\.local.
+SITES_BASE_DOMAIN_REGEX=$(printf '%s' "$SITES_BASE_DOMAIN" | sed 's/\./\\./g')
 export SITES_BASE_DOMAIN_REGEX
 
-envsubst '${SITES_BASE_DOMAIN_REGEX}' \
-  < /etc/nginx/static-sites.conf.template \
-  > /etc/nginx/conf.d/default.conf
+# Only substitute our generated variable. Do NOT substitute $site or $uri,
+# they are Nginx variables used at request time.
+envsubst '${SITES_BASE_DOMAIN_REGEX}'   < /etc/nginx/static-sites.conf.template   > /etc/nginx/conf.d/default.conf
 
 echo "Rendered static-sites nginx config for *.${SITES_BASE_DOMAIN}"
